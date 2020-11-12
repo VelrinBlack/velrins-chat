71bf7f2554fb71392edcfcf2f6be68ad9422a6fd
velrinblack <velrinblack@gmail.com>
2020-11-12 06:46:41 +0100

    Connect login form to the backend

--- components/Home/FormSection/LoginForm/LoginForm.js
+++ components/Home/FormSection/LoginForm/LoginForm.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const RegisterForm = ({ changeToRegister }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const [emptyFields, setEmptyFields] = useState(false);
-  const [requestStatus, setRequestStatus] = useState(null);
+  const [serverResponse, setServerResponse] = useState(null);
 
   const handleInputChange = ({ target }) => {
     switch (target.name) {
@@ -22,7 +23,25 @@ const RegisterForm = ({ changeToRegister }) => {
     setEmptyFields(!email || !password);
 
     if (email && password) {
-      console.log('Everything is ok.');
+      setServerResponse('loading');
+
+      axios
+        .post(`${process.env.BACKEND_URL}/api/user/authenticate`, {
+          email,
+          password,
+        })
+        .then((res) => {
+          if (res.data.token) {
+            localStorage.setItem('token', res.data.token);
+          } else if (res.data === 'Invalid email or password') {
+            setServerResponse('Invalid email or password');
+          } else {
+            setServerResponse('error');
+          }
+        })
+        .catch(() => {
+          setServerResponse('error');
+        });
     }
   };
 
@@ -44,34 +63,37 @@ const RegisterForm = ({ changeToRegister }) => {
         value={password}
       />
 
-      {emptyFields ? (
-        <p className='emptyFieldsError'>Please fill empty fields</p>
-      ) : null}
+      {emptyFields ? <p className='emptyFieldsError'>Please fill empty fields</p> : null}
 
-      {requestStatus === 'loading' ? (
+      <button type='submit' aria-label='submit' style={!emptyFields ? { marginTop: '30px' } : null}>
+        <span>Sign in</span>
+      </button>
+
+      {serverResponse === null ? null : serverResponse === 'loading' ? (
         <div className='loading'>
           <div className='dot1'></div>
           <div className='dot2'></div>
           <div className='dot3'></div>
         </div>
-      ) : requestStatus === 'error' ? (
+      ) : serverResponse === 'Invalid email or password' ? (
         <div className='error'>
           <img src='/images/error.svg' alt='error' />
-          <p>Something went wrong.</p>
+          <p>
+            Invalid email or password <span onClick={changeToRegister}>Sign up</span>
+          </p>
         </div>
       ) : (
-        <button
-          type='submit'
-          aria-label='submit'
-          style={!emptyFields ? { marginTop: '30px' } : null}
-        >
-          <span>Sign in</span>
-        </button>
+        <div className='error'>
+          <img src='/images/error.svg' alt='error' />
+          <p>Something went wrong</p>
+        </div>
       )}
 
-      <p>
-        Don't have an account? <span onClick={changeToRegister}>Sign up</span>
-      </p>
+      {serverResponse === null ? (
+        <p>
+          Already have an account? <span onClick={changeToRegister}>Sign up</span>
+        </p>
+      ) : null}
     </form>
   );
 };
